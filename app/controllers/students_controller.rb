dfbf05c8e800490d63a8a311afefc3d70b0a8a62
Chad Lengefeld <chadlengefeld@gmail.com>
2020-09-15 03:26:50 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,8 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
   end
   
   def show
