e95d95dd7c8847c85a8964847d8276865237558f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-09-15 03:28:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect to '/students/show'
+    redirect students_path
   end
 
   def show
